3d7c3c31d6d930d13812016026eeec3dd99c3cb2
Tamer Shlash <mr.tamershlash@gmail.com>
2013-11-09 21:45:24 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BloggersWithoutBorders::Application.config.secret_token = '390cb24f58211d0d6c05b30a12c613e26ba26de81c947ca3eaafa67f1733f2a24a9db6b9027267a2ecc61531e06d472234903bbcdf9361a1db9fa8a93d74c29e'
