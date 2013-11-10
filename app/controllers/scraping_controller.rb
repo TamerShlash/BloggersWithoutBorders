2629076fbda839c39f5ffc06e4a79016bd040c38
Tamer Shlash <mr.tamershlash@gmail.com>
2013-11-10 18:46:46 +0200

    Moved the sqlite to development gems group

--- app/controllers/scraping_controller.rb
+++ app/controllers/scraping_controller.rb
@@ -6,15 +6,23 @@ class ScrapingController < ApplicationController
   end
 
   def parse
+    if params[:url].nil?
+      redirect_to root_path
+    end
     html = Nokogiri::HTML(open(params[:url]).read)
 
     @images = []
-    html.css('img').each { |image| @images.append image.attribute('src') }
+    html.css('*[id*=post-] img').each do |image|
+      if image and image.attribute('src')
+        parsed_url = URI.parse(image.attribute('src'))
+        p = ((parsed_url.port == 80 or parsed_url.port == 443)? '' : parsed_url.port.to_s)
+        @images.append ((parsed_url.scheme || 'http') + '://' + parsed_url.host + p + parsed_url.path)
+      end
+    end
     @images.uniq!
 
     @tags = []
-    html.css('*[rel~=tag]').each { |tag| @tags.append tag.inner_text }
-    @tags.uniq!
-
+    html.css('*[rel~=tag]').each { |tag| @tags.append '#' + tag.inner_text.downcase.gsub(/[^[:alnum:]]/,' ').strip.gsub(/\s/,'_').gsub(/_{2,}/,'_') }
+    @tags.uniq!.sort!
   end
 end
